eef766ea551fc38a94cb8b20cbb8f9bcc958d1bb
Jessie Hernandez <github@jessiehernandez.com>
2020-12-09 17:00:17 +0100

    Fixed the variables that are checked in the pre install script

--- .github/travis/before-install.sh
+++ .github/travis/before-install.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
 
-env
-if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-	sudo apt-get update -qq
-	sudo apt-get -y install bison \
-				autotools-dev \
-				libncurses5-dev \
-				libevent-dev \
-				pkg-config \
-				libutempter-dev \
-				build-essential
+if [ "$RUNNER_OS" = "Linux" ]; then
+    sudo apt-get update -qq
+    sudo apt-get -y install bison \
+                autotools-dev \
+                libncurses5-dev \
+                libevent-dev \
+                pkg-config \
+                libutempter-dev \
+                build-essential
 
-	if [ "$BUILD" = "musl" -o "$BUILD" = "musl-static" ]; then
-		sudo apt-get -y install musl-dev \
-					musl-tools
-	fi
+    if [ "$BUILD" = "musl" -o "$BUILD" = "musl-static" ]; then
+        sudo apt-get -y install musl-dev \
+                    musl-tools
+    fi
 fi
 
-if [ "$TRAVIS_OS_NAME" = "freebsd" ]; then
-	sudo pkg install -y \
-		automake \
-		libevent \
-		pkgconf
+if [ "$RUNNER_OS" = "freebsd" ]; then
+    sudo pkg install -y \
+        automake \
+        libevent \
+        pkgconf
 fi
